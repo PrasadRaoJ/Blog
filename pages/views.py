7b60eba146ba4d862edcceb56516e33727bef6d3
Prasad Rao <jammunaprasadrao@gmail.com>
2021-04-27 16:49:01 +0530

    adding project

---
+++ pages/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,redirect
+
+
+# Create your views here.
+
+from .models import Contact
+
+def home(request):
+    return render(request, 'blog/blog.html')
+
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        c = Contact(name=name, email=email, subject=subject, message=message)
+        c.save()
+        return redirect('contact')
+
+    return render(request, 'pages/contact.html')
+    
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+    #return render(request, 'users/signup.html')
+    #return render(request, 'blog/post.html')
